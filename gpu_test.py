8f73e2bf7ca4ce92f6d0fb4f6ce9a4e1a09c6b3c
Sebastianrix <82590812+Sebastianrix@users.noreply.github.com>
2025-06-03 10:06:06 +0200

    removed debugging and failed experiments

--- gpu_test.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print("\nGPU Devices:")
-print(tf.config.list_physical_devices('GPU'))
-print("\nAll Devices:")
-print(tf.config.list_physical_devices()) 
--- quick_gpu_test.py
+++
@@ -1,4 +0,0 @@
-import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("\nNum GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-print("\nDevices:", tf.config.list_physical_devices()) 
